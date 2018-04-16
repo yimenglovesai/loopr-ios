8b32b1141ca68ad3cbbec634711bdafc55be8727
Carina Shen <yimenglovesai@gmail.com>
2018-04-15 22:53:27 -0700

    added scan view into UI

--- loopr-ios/QRCode/ScanQRCodeViewController.swift
+++ loopr-ios/QRCode/ScanQRCodeViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import QRCodeReader
 
 class ScanQRCodeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var scanCameraView: QRCodeReaderView! {
+        didSet {
+            scanCameraView.setupComponents(showCancelButton: false, showSwitchCameraButton: false, showTorchButton: false, showOverlayView: true, reader: reader)
+        }
+    }
+    
+    lazy var reader: QRCodeReader = QRCodeReader()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +31,47 @@ class ScanQRCodeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard checkScanPermissions(), !reader.isRunning else { return }
+        
+        reader.didFindCode = { result in
+            print("Completion with result: \(result.value) of type \(result.metadataType)")
+        }
+        
+        reader.startScanning()
+        
+    }
+    
+    private func checkScanPermissions() -> Bool {
+        do {
+            return try QRCodeReader.supportsMetadataObjectTypes()
+        } catch let error as NSError {
+            let alert: UIAlertController
+            
+            switch error.code {
+            case -11852:
+                alert = UIAlertController(title: "Error", message: "This app is not authorized to use Back Camera.", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
+                    DispatchQueue.main.async {
+                        if let settingsURL = URL(string: UIApplicationOpenSettingsURLString) {
+                            UIApplication.shared.openURL(settingsURL)
+                        }
+                    }
+                }))
+                
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            default:
+                alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            }
+            
+            present(alert, animated: true, completion: nil)
+            
+            return false
+        }
     }
 
 }
